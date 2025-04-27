f2372996e62a69cf6fd9694e819b796f1be27b6a
rifkiilhamlutfika-dev <rifkiilhamlutfika.dev@gmail.com>
2025-04-27 08:36:10 +0700

    Setup Project

---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const port = 8008 || 3000;
+
+const app = express();
+
+// connect to mongodb --
+mongoose
+  .connect("mongodb://127.0.0.1/shop_db")
+  .then((result) => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Shop app listening on http://localhost:${port}`);
+});
